8d6084a5f1a479554c9369f9408a6dd808956ee8
Bahailu Abera <aberabahailu4@gmail.com>
2023-06-26 23:45:15 +0300

    fix: document the methods

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -33,18 +33,21 @@ class Server:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+                self.__dataset = dataset[1:]
 
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            assert isinstance(page, int) and page > 0
-            assert isinstance(page_size, int) and page_size > 0
+        """
+        Method to get a page
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-            start, end = index_range(page, page_size)
-            data = self.dataset()
+        start, end = index_range(page, page_size)
+        data = self.dataset()
 
-            if end >= len(data):
-                return []
+        if end >= len(data):
+            return []
 
-            return data[start:end]
+        return data[start:end]
